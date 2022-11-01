6adb3c20f722a1bec0171b95388f6f56f1c77150
Administrator <Administrator@User-2022XSAMWU>
2022-11-01 11:57:36 +0900

    숫자는 자동으로 int

---
+++ Project_Nov/src/LongExample.java
@@ -0,0 +1,16 @@
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 200L;
+//		long var3 = 1000000000;
+//		숫자는 자동으로 int 타입으로 인식
+//		long var3 = 10000000000000;
+		String var3 = "10000000000000";
+		long var4 = 10000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+}
