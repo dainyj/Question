2c6e7658b51d37ba12e28ec651b31c2e14550c5f
Administrator <Administrator@User-2022XSAMWU>
2022-11-01 10:32:23 +0900

    ㅎㅎ

---
+++ Project_Nov/src/CharExample.java
@@ -0,0 +1,14 @@
+public class CharExample {
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 65;
+		int a3 = 'A';
+		int a4 = 'a';		
+		//char은 문자, int는 정수, A=65(ASCII코드 참고) a=97
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(a3);
+		System.out.println(a4);		
+	}
+}
