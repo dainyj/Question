819a7ad2c3021779947cdadb5e97a43a77530fe7
Administrator <Administrator@User-2022XSAMWU>
2022-11-01 12:21:42 +0900

    연산자 + - * / %

---
+++ Project_Nov/src/OperatorExample.java
@@ -0,0 +1,30 @@
+public class OperatorExample {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 4;
+		int a = 5, b = 6, c = 7;
+		int res1 = 0, res2 = 0;
+		
+		a= b = c =8;
+		res1 = 100 * 2 + 7 % 5;
+		res2 = 100 * ( 2 + 7 ) % 5;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.print("\n");
+		System.out.println(res1);
+		System.out.println(res2);
+		
+//		int res3 = 7%5;
+//		int res4 = (2+7)%5;
+//		int res5 = 100 * 2 + res3;
+//		int res6 = 100 * res4;
+		
+//		System.out.println(res3);
+//		System.out.println(res4);
+		
+	}
+}
