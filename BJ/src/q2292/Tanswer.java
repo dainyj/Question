e1117b2aed62a86b811a8e15fd3976d9bb7dd0b4
dainyj <117058134+dainyj@users.noreply.github.com>
2022-12-06 23:30:50 +0900

    백준 q2292 t-answer

---
+++ BJ/src/q2292/Tanswer.java
@@ -0,0 +1,23 @@
+package q2292;
+
+import java.util.Scanner;
+
+public class Tanswer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+//		System.out.println(N);
+
+		int r = 1, m = 1;
+		while (true) {
+			if (N <= r) {
+				break;
+			}
+			r = r + 6 * m++;
+		}
+		System.out.println(m);
+	}
+}
