cbc576634ed644fc9a5cf6511a33bcef4b876b1f
dainyj <117058134+dainyj@users.noreply.github.com>
2022-12-06 23:37:37 +0900

    q112

---
+++ JO/Operator/q112_op/Main.java
@@ -0,0 +1,15 @@
+package q112_op;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		sc.close();
+//		System.out.println(n1 + " " + n2);
+		System.out.printf("%d / %d = %d...%d", n1, n2, n1 / n2, n1 % n2);
+	}
+}
