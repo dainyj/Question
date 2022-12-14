24dbde1a7ebfc18436e5dab17b3feabe05b73dcf
dainyj <jdatwo@naver.com>
2022-12-14 18:08:36 +0900

    q1193 bj

---
+++ BJ/src/q1193/Main.java
@@ -0,0 +1,26 @@
+
+package q1193;
+
+//import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int inp = sc.nextInt();
+//		sc.close();
+//		System.out.println(inp);
+		int n=0;
+		int line=1;
+		if(line%2!=0) {
+			System.out.println("'"+line+"'");
+			
+		}
+
+		
+		
+		
+		
+
+	}
+
+}
