4c2604c526ddfec5312502a15967a8595eaf5dd7
dainyj <jdatwo@naver.com>
2023-02-15 18:07:37 +0900

    jsp 모델2 게시판

---
+++ MustHaveJSP/src/main/java/model2/mvcboard/PassController.java
@@ -0,0 +1,25 @@
+package model2.mvcboard;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PassController
+ */
+@WebServlet("/mvcboard/pass.do")
+public class PassController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("mode", request.getParameter("mode"));
+		request.getRequestDispatcher("/14MVCBoard/Pass.jsp").forward(request, response);
+
+	}
+}
--- MustHaveJSP/src/main/java/utils/JSFunction.java
+++ MustHaveJSP/src/main/java/utils/JSFunction.java
@@ -1,4 +1,7 @@
 package utils;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -26,4 +29,33 @@ public class JSFunction {
 			out.println(script);
 		} catch (Exception e) {}
 	}
+	
+	//메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = "" //삽입할 자바스크립트 코드
+							+ "<script>"
+							+ "		alert('" + msg+ "');"
+							+ "		location.href='" + url + "';"
+							+ "</script>";
+			writer.print(script);
+		} catch (Exception e) {}
+	}
+	
+	//메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+							+ "<script>"
+							+ "		alert('" + msg + "');"
+							+ "		history.back();"
+							+ "</script>";
+			writer.print(script);
+		} catch (Exception e) {}
+	}
+	
 }
